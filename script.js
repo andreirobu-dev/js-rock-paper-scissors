6c049e831eae967b966a82ce6070d71a1a08ea00
andreirobu-dev <dev.andreirobu@pm.me>
2026-03-24 12:34:11 +0200

    Display game text in a div

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let playerScore = 0;
 let computerScore = 0;
 
 const choiceButtons = document.querySelectorAll(".choice");
+const result = document.createElement("div");
+const body = document.querySelector("body");
+const playerPick = document.createElement("div");
+const computerPick = document.createElement("div");
+const output = document.createElement("div");
+const score = document.createElement("div");
 
 choiceButtons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -11,23 +17,27 @@ choiceButtons.forEach((button) => {
 
 // Play a round, check for win conditions
 function playRound(playerChoice, computerChoice) {
-    console.log(`Player picked.. ${playerChoice}`);
-    console.log(`Computer picked.. ${computerChoice}`);
+    playerPick.textContent = `Player picked.. ${playerChoice}`;
+    computerPick.textContent = `Computer picked.. ${computerChoice}`;
 
     if (playerChoice === computerChoice) {
-        console.log("Tie!");
+        result.textContent = "Tie!";
     } else if ((playerChoice === "rock" && computerChoice === "scissors")
         || (playerChoice === "paper" && computerChoice === "rock")
         || (playerChoice === "scissors" && computerChoice === "paper")) {
         playerScore++;
-        console.log("Player Wins!");
+        result.textContent = "Player wins!";
     } else {
         computerScore++;
-        console.log("Computer Wins!");
+        result.textContent = "Computer wins!";
     }
 
-    console.log(`Score: Player ${playerScore} - Computer ${computerScore}`);
-    console.log("");
+    score.textContent = `Score: Player ${playerScore} - Computer ${computerScore}`;
+    output.appendChild(playerPick);
+    output.appendChild(computerPick);
+    output.appendChild(result);
+    output.appendChild(score);
+    body.appendChild(output);
 }
 
 // Get a random choice from the computer
